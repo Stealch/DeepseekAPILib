2deb95bc024b7812e773500f972b1269f54a75a6
Vladimir <vokzalka@ya.ru>
2026-01-23 17:23:14 +0300

    Очистка

--- ChatMessage.cs
+++ ChatMessage.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DeepseekAPILib
+﻿namespace DeepseekAPILib
 {
     /// <summary>
     /// Represents a message in a chat conversation
--- DeepSeekAPI.cs
+++ DeepSeekAPI.cs
@@ -1,11 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections.Generic;
-using System.Linq;
-using Newtonsoft.Json;
-using DeepseekAPILib.Models;
 
 namespace DeepseekAPILib
 {
